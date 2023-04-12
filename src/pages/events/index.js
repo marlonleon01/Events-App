44b8099240f8bfe0d5531e50877038bb56daefa6
Marlon Leon <ghostveene@gmail.com>
2023-04-12 10:40:20 -0400

    added events to page

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,5 +1,21 @@
 export default function Event() {
     return (
-        <h1>Events</h1>
+        <>
+            <h1>Events Page</h1>
+            <div>
+                <a href="">
+                    <img />
+                    <h2>Events in London</h2>
+                </a>
+                <a href="">
+                    <img />
+                    <h2>Events in San Fransisco</h2>
+                </a>
+                <a href="">
+                    <img />
+                    <h2>Events in Barcelona</h2>
+                </a>
+            </div>
+        </>
     )
 }
