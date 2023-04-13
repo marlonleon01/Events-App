6dd1ae61f44f619679b0e3c276da427e7db60a8b
Marlon Leon <ghostveene@gmail.com>
2023-04-13 10:24:11 -0400

    fixed bug with link

--- src/pages/events/[category]/index.js
+++ src/pages/events/[category]/index.js
@@ -7,7 +7,7 @@ export default function EventsCategoryPage({data, pageName}) {
             <h1>Events in {pageName}</h1>
             <div>
                 {data.map(ev => (
-                        <Link key={ev.id} href={`/events/${ev.city}/${ev.id}}`}>
+                        <Link key={ev.id} href={`/events/${ev.city}/${ev.id}`}>
                             <Image src={ev.image} alt={ev.title} width={300} height={300}/>
                             <h2>{ev.title}</h2>
                             <p>{ev.description}</p>
