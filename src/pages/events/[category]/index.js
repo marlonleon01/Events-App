3f502cc9d1df88878c07e647302b50af4377d9c6
Marlon Leon <ghostveene@gmail.com>
2023-04-13 06:26:16 -0400

    created for individual city

---
+++ src/pages/events/[category]/index.js
@@ -0,0 +1,33 @@
+export default function EventsCategoryPage() {
+    return (
+        <>
+            <h1>Events in London</h1>
+            <div>
+                <a href="">
+                    <img /> 
+                    <h2>Event 1</h2>
+                </a>
+                <a href="">
+                    <img /> 
+                    <h2>Event 2</h2>
+                </a>
+                <a href="">
+                    <img /> 
+                    <h2>Event 3</h2>
+                </a>
+                <a href="">
+                    <img /> 
+                    <h2>Event 4</h2>
+                </a>
+                <a href="">
+                    <img /> 
+                    <h2>Event 5</h2>
+                </a>
+                <a href="">
+                    <img /> 
+                    <h2>Event 6</h2>
+                </a>
+            </div>
+        </>
+    )
+}
