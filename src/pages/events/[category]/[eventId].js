1fb79e1d12dd585e389dbe93dcbfc6e2c4aeb30b
Marlon Leon <ghostveene@gmail.com>
2023-04-13 10:23:47 -0400

    rendering individual events

--- src/pages/events/[category]/[eventId].js
+++ src/pages/events/[category]/[eventId].js
@@ -1,5 +1,39 @@
-export default function EventPage() {
+import Image from "next/image"
+
+export default function EventPage({data}) {
     return (
-        <h1>Our Single Event</h1>
+        <div>
+            <Image src={data.image} width={500} height={300} alt={data.title}/>
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+        </div>
     )
+}
+
+export async function getStaticPaths() {
+    const {allEvents} = await import("/data/data.json")
+    const allPaths = allEvents.map(path => {
+        return {
+            params: {
+                category: path.city,
+                eventId: path.id
+            }
+        }
+    })
+    
+    return {
+        paths: allPaths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps(context) {
+    console.log(context)
+    const {allEvents} = await import("/data/data.json")
+    const id = context.params.eventId
+    const eventData = allEvents.find(ev => ev.id === id)
+
+    return {
+        props: {data: eventData}
+    }
 }
