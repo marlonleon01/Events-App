f8b25cf04cea17b8ed7d72c43199db356b46c314
Marlon Leon <ghostveene@gmail.com>
2023-04-13 12:44:14 -0400

    wrapped in div

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,23 +4,25 @@ import Image from "next/image"
 export default function Header() {
     return (
 		<header>
-            <div className="topNav">
-                <Image alt="logo" src={"/image/logo_black.png"} width={50} height={50}/>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link href="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link href="/events" >Events</Link>
-                        </li>
-                        <li>
-                            <Link href="/about-us" >About us</Link>
-                        </li>
-                    </ul>
-                </nav>
+            <div>
+                <div className="topNav">
+                    <Image alt="logo" src={"/image/logo_black.png"} width={50} height={50}/>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link href="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link href="/events" >Events</Link>
+                            </li>
+                            <li>
+                                <Link href="/about-us" >About us</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <h1>Sed ut perspiciatis unde omnis</h1>
             </div>
-            <h1>Sed ut perspiciatis unde omnis</h1>
         </header>
     )
 }
