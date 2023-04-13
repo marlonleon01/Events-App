db3729a9b35a1ccd521d6f93fb338085100404be
Marlon Leon <ghostveene@gmail.com>
2023-04-13 09:12:15 -0400

    added getStaticPaths and getStaticProps

--- src/pages/events/[category]/index.js
+++ src/pages/events/[category]/index.js
@@ -31,3 +31,29 @@ export default function EventsCategoryPage() {
         </>
     )
 }
+
+export async function getStaticPaths() {
+    const {events_categories} = await import("/data/data.json")
+    const allPaths = events_categories.map(ev => {
+        return {
+            params: {
+                category: ev.id.toString()
+            }
+        }
+    })
+
+        return {
+            paths: allPaths,
+            fallback: false
+        }
+}
+
+export async function getStaticProps(context) {
+    const {allEvents} = await import("/data/data.json")
+    const id = context?.params.category
+    const data = allEvents.filter(ev => ev.city === id)
+    
+    return {
+        props: {data}
+    }
+}
