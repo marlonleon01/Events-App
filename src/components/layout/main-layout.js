8df25d101474bd7f9af640badc2c1033253e6b9e
Marlon Leon <ghostveene@gmail.com>
2023-04-13 10:43:48 -0400

    created main-layout which takes the header and footer and children props

---
+++ src/components/layout/main-layout.js
@@ -0,0 +1,12 @@
+import Footer from "../Footer/footer";
+import Header from "../Header/header";
+
+export default function MainLayout({children}) {
+    return (
+        <>
+            <Header />
+            {children}
+            <Footer />
+        </>
+    )
+}
