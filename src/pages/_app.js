50df15cfd88b646e255cb17387ebfd65f9d0f998
Marlon Leon <ghostveene@gmail.com>
2023-04-13 10:44:14 -0400

    added mainlayout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,11 @@
+import MainLayout from "@/components/layout/main-layout"
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+		<>
+			<MainLayout>
+      			<Component {...pageProps} />
+			</MainLayout>
+		</>
+    )
 }
