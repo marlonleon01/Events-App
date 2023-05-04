43f080913ccfc3c0baca67fa6cd881661d2640bd
Marlon Leon <ghostveene@gmail.com>
2023-05-04 10:03:28 -0400

    added class to div for styling

--- src/components/Events/catEvent.js
+++ src/components/Events/catEvent.js
@@ -5,7 +5,7 @@ export default function CatEvent({data, pageName}) {
     return (
         <div className="cat_events">
             <h1>Events in {pageName}</h1>
-            <div>
+            <div className="content">
                 {data.map(ev => (
                         <Link key={ev.id} href={`/events/${ev.city}/${ev.id}`} className="card">
                             <Image src={ev.image} alt={ev.title} width={300} height={300}/>
