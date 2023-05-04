3e63f4a4418a7f8949e65147f2d13a16986cad91
Marlon Leon <ghostveene@gmail.com>
2023-05-04 10:24:21 -0400

    added class to main div and form element for styling

--- src/components/Events/singe-event.js
+++ src/components/Events/singe-event.js
@@ -2,12 +2,15 @@ import Image from "next/image"
 
 export default function SingleEvent({data}) {
     return (
-        <div>
+        <div className="event_single_page">
             <h1>{data.title}</h1>
             <Image src={data.image} width={1000} height={500} alt={data.title}/>
             <p>{data.description}</p>
-            <input type="email" />
-            <button>Submit</button>
+            <form className="email_registration">
+                <label>Get Registered for this event!</label>
+                <input type="email" />
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
